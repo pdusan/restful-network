355f774c3764e6b592a9d6e1f96a252cb2e818cf
Dusan Popovic <34303979+pdusan@users.noreply.github.com>
2022-05-09 01:00:04 +0000

    started with flask

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from rdflib import Graph
+from flask import Flask
 
 g = Graph().parse("v11.owl", format='xml')
 
-q = """
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+app = Flas(__name__)
 
-    SELECT ?name
-    WHERE {
-        ?p rdf:type foaf:User .
-    }
-"""
+@app.route('/users')
+def listUsers():
+    q = """
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX mst: <https://mis.cs.univie.ac.at/ontologies/2021SS/mst#>
+        PREFIX ma-ont: <http://www.w3.org/ns/ma-ont>
 
-print(g.serialize(format="turtle"))
+        SELECT DISTINCT ?name
+        WHERE {
+            ?name a mst:User
+        }
+    """
+    res = g.query(q)
+    return res
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8080')
